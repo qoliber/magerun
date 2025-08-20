babc8c49854b19e5891e2cfdc7a65145d9d61c29
Lukasz Owczarczuk <lowczarczuk@qoliber.com>
2025-08-21 00:27:21 +0200

    [master] 2025-08-21 00:27:21
    [master] 2025-08-21 00:27:21

---
+++ bin/scripts/qoliber-media-without-catalog-dump.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Created by Qoliber
+#
+# @author Lukasz Owczarczuk <lowczarczuk@qoliber.com>
+
+mediaDir='pub/media'
+resolvedDir=$(readlink -f "$mediaDir")
+
+if [ ! -L "$mediaDir" ]; then
+  resolvedDir="$mediaDir"
+fi
+
+if [ ! -d "$resolvedDir" ]; then
+  echo "Error: '$resolvedDir' does not exist"
+  exit 1
+fi
+
+dumpDir='var/dump'
+archiveName="$dumpDir/media.tgz"
+
+mkdir -p "$dumpDir"
+rm -f "$archiveName"
+
+tar --exclude='*cache*' --exclude='*-bak' --exclude='*_bak' --exclude='*_backup' \
+    --exclude='*-backup' --exclude='*_tmp' --exclude='*.tmp' --exclude='*_old' \
+    --exclude='*.old' --exclude='*-old' --exclude='amfeed' --exclude='sitemap' \
+    --exclude='tmp' --exclude='.[^/]*' --exclude='css' --exclude='css_secure' \
+    --exclude='js' --exclude='test' --exclude='js_secure' --exclude='import' \
+    --exclude='*.sql' --exclude='*.log' --exclude='*.tar' --exclude='*.gz' \
+    --exclude='*.zip' --exclude='catalog' --exclude='ftp-upload' --exclude='importexport' \
+    --exclude='pdf' --exclude='csv' --dereference --no-same-owner \
+    -zcvf "$archiveName" -C "$(dirname "$resolvedDir")" "$(basename "$resolvedDir")"
